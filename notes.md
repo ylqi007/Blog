b66378b74a86704e6e3b76b85b61b34ff0a9975a
ylqi007 <yunlongqi@my.unt.edu>
2020-11-15 21:02:53 -0600

    20201115, LogAspect

---
+++ notes.md
@@ -0,0 +1,22 @@
+[toc]
+
+
+
+## Error and Exception
+
+* `src/main/java/com/ylqi007/blog/handler/ControllerExceptionHandler`
+
+  在 `ControllerExceptionHandler` 中有 `@ControllerAdvice` 和 `@ExceptionHandler` 两个 annotation。
+
+  > The `@ControllerAdvice` is an annotation, to handle the exceptions globally.
+  >
+  > The `@ExceptionHandler` is an annotation used to handle the specific exceptions and sending the custom responses to the client.
+
+
+
+
+
+## Reference
+
+* [Exception Handling in Spring MVC](https://spring.io/blog/2013/11/01/exception-handling-in-spring-mvc)
+* 
--- src/main/java/com/ylqi007/blog/web/IndexController.java
+++ src/main/java/com/ylqi007/blog/web/IndexController.java
@@ -3,17 +3,19 @@ package com.ylqi007.blog.web;
 import com.ylqi007.blog.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
-    public String index() {
+    @GetMapping("/{id}/{name}")
+    public String index(@PathVariable Integer id, @PathVariable String name) {
 //        int i = 9 / 0;
-        String blog = null;
-        if (blog == null) {
-            throw new NotFoundException("Blog Not Found.");
-        }
+//        String blog = null;
+//        if (blog == null) {
+//            throw new NotFoundException("Blog Not Found.");
+//        }
+        System.out.println(" ======== IndexController.index() ==========");
         return "index";
     }
 }
