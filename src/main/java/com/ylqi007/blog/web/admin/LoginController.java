a27faeb7da0ff2d4c1dfda8fd363825ac692bdfe
ylqi007 <yunlongqi@my.unt.edu>
2020-11-25 14:06:22 -0600

    20201125, login

--- src/main/java/com/ylqi007/blog/web/admin/LoginController.java
+++ src/main/java/com/ylqi007/blog/web/admin/LoginController.java
@@ -5,7 +5,6 @@ import com.ylqi007.blog.po.User;
 import com.ylqi007.blog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -16,7 +15,7 @@ import javax.servlet.http.HttpSession;
 public class LoginController {
 
     @Autowired
-    private UserService userService;
+    private UserService userService;    // userService to operate with MySQL database
 
     @GetMapping // default, i.e. when getting "/admin", it will call this function and return "admin/login"
     public String loginPage() {
